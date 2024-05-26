069c7a479aca662bbac974c72391e52366e44ae2
Sergey Evstegneiev <serg123e@gmail.com>
2024-05-26 23:41:21 +0200

    Update train.py

--- 02-experiment-tracking/homework/train.py
+++ 02-experiment-tracking/homework/train.py
@@ -1,3 +1,4 @@
+import mlflow
 import os
 import pickle
 import click
@@ -18,16 +19,24 @@ def load_pickle(filename: str):
     help="Location where the processed NYC taxi trip data was saved"
 )
 def run_train(data_path: str):
+    with mlflow.start_run(): 
 
-    X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
-    X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
+        X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
+        X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
 
-    rf = RandomForestRegressor(max_depth=10, random_state=0)
-    rf.fit(X_train, y_train)
-    y_pred = rf.predict(X_val)
+        rf = RandomForestRegressor(max_depth=10, random_state=0)
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_val)
 
-    rmse = mean_squared_error(y_val, y_pred, squared=False)
+        rmse = mean_squared_error(y_val, y_pred, squared=False)
+        mlflow.log_metric("rmse", rmse)
 
 
+
+mlflow.set_tracking_uri("sqlite:////home/sir/mlops/mlops-zoomcamp/02-experiment-tracking/homework/mlflow.db")
+mlflow.set_experiment("hw2-train")
+
+mlflow.autolog()
 if __name__ == '__main__':
     run_train()
+

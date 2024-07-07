7399e35eaa6c18f39ac4292c11c2c9c801f345b8
Sergey Evstegneiev <serg123e@gmail.com>
2024-07-07 22:44:22 +0200

    Make input and output paths configurable

--- 06-best-practices/homework/batch.py
+++ 06-best-practices/homework/batch.py
@@ -20,10 +20,22 @@ def prepare_data(df, categorical):
     
     return df
 
-def main(year,month):
-    input_file = f'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{year:04d}-{month:02d}.parquet'
-    output_file = f'output/yellow_tripdata_{year:04d}-{month:02d}.parquet'
 
+def get_input_path(year, month):
+    default_input_pattern = 'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{year:04d}-{month:02d}.parquet'
+    input_pattern = os.getenv('INPUT_FILE_PATTERN', default_input_pattern)
+    return input_pattern.format(year=year, month=month)
+
+
+def get_output_path(year, month):
+    default_output_pattern = 's3://nyc-duration-prediction-serg123e/taxi_type=fhv/year={year:04d}/month={month:02d}/predictions.parquet'
+    output_pattern = os.getenv('OUTPUT_FILE_PATTERN', default_output_pattern)
+    return output_pattern.format(year=year, month=month)
+
+
+def main(year, month):
+    input_file = get_input_path(year, month)
+    output_file = get_output_path(year, month)
 
     with open('model.bin', 'rb') as f_in:
         dv, lr = pickle.load(f_in)
